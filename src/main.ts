c5c669f672e53f41be305caf62756bff6a13ddfd
John Haugeland <stonecypher@gmail.com>
2020-01-01 11:09:25 -0800

    Basic steps on action (glob, file types,) more cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,39 @@
-import * as core from '@actions/core'
-import {wait} from './wait'
 
-async function run(): Promise<void> {
-  try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`)
+import * as core from '@actions/core';
+import {wait}    from './wait';
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
 
-    core.setOutput('time', new Date().toTimeString())
-  } catch (error) {
-    core.setFailed(error.message)
-  }
+
+
+
+function timestring() {
+  return new Date().toTimeString();
 }
 
-run()
+
+
+
+
+async function run(): Promise<void> {
+
+  try {
+    const ms: number = parseInt(core.getInput('milliseconds'), 10);
+
+    core.debug(`Waiting ${ms} milliseconds ...`);
+    core.debug(timestring());
+
+    await wait(ms);
+    core.debug(timestring());
+    core.setOutput('time', timestring());
+
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+
+}
+
+
+
+
+
+run();
